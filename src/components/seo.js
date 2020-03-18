3930f6e30bb968f182f789686b10f3032388b9ec
Iann Santos <iannsantos8@gmail.com>
2020-03-17 22:00:25 -0300

    refactor: change default image

--- src/components/seo.js
+++ src/components/seo.js
@@ -28,7 +28,7 @@ function SEO({ description, lang, meta, title, image }) {
 
   const metaDescription = description || site.siteMetadata.description
   const url = site.siteMetadata.siteUrl
-  const ogImage = `${url}${image || '/assets/img/hello-world.png'}`
+  const ogImage = `${url}${image || '/assets/img/code.png'}`
 
   return (
     <Helmet
